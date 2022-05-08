42a8ea702f98f57231bd6902ac0968ee3d24ac24
SoraY677 <SoraY677@outlook.com>
2022-05-08 18:00:41 +0900

    Task model

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,17 @@
+package todolist
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import spark.Spark.get
 
-fun main(args: Array<String>) {
-	get("/Hello") {
-		request, response -> 
-			val name:String = request.queryParams("name") ?: "World"
-			"Hello, ${name}"
-	}
+fun main(args: Array<String>){
+	val objectMapper = ObjectMapper().registerKotlinModule()
+	val hoge = "hoge"
+	get("/tasks", 
+	{ req, res -> listOf(
+		Task(1, "クリーニングに出す", false),
+		Task(2, "住民票を出す", true)
+	)}, 
+	objectMapper::writeValueAsString
+	)
 }
